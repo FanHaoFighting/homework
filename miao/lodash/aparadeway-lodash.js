cd71c09c147d5c55c90c008dbb73bb7b09b39007
aparadeway <10yuedefeng@163.com>
2018-07-12 15:40:13 +0800

    Add files via upload

---
+++ miao/lodash/aparadeway-lodash.js
@@ -0,0 +1,12 @@
+var aparadeway = {
+  chunk: function (array, size){
+    var myCars = []
+    for (var i = 0; i < array.length; i = i + size){
+      myCars.push(array.slice(i,i+size))
+    }
+    return myCars
+  }
+
+}
+
+
