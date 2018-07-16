1fb23b2d6be6ee087ea58556becffdfe0152e776
aparadeway <10yuedefeng@163.com>
2018-07-16 15:16:19 +0800

    Add files via upload

--- miao/lodash/aparadeway-lodash.js
+++ miao/lodash/aparadeway-lodash.js
@@ -1,11 +1,39 @@
 var aparadeway = {
   chunk: function (array, size){
-    var myCars = []
-    for (var i = 0; i < array.length; i = i + size){
-      myCars.push(array.slice(i,i+size))
+    var newArr = [];
+    var k = 0;
+    for(var i = 0;i < array.length;i++){
+      if(newArr[k] != undefined && newArr[k].length == size){
+        k ++;
+      }
+      if(newArr[k] == undefined){
+        newArr[k] = [];
+      }
+      newArr[k].push(array[i]);
     }
-    return myCars
+    return newArr
+  },
+  compact: function (array){
+    var pointer;
+    var sum = 0;
+    for(var i = 0;i < array.length;i++){
+      if(array[i] == null || array[i] == 0 || array[i] == "" || !array[i] || array[i] != array[i])
+      {
+        if(pointer == undefined){
+          pointer = i;
+        }
+        sum++;
+      }
+      else{
+        if(pointer != undefined){
+          array[pointer] = array[i];
+          pointer++
+        }
+      }
+    }
+    array.length = array.length - sum;
   }
+
 
 }
 
